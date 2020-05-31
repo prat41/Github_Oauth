acb54b3554cd3fec4ca6102f9b749ec85a1d1e10
Prathamesh Gogawale <40314490+prat41@users.noreply.github.com>
2020-05-31 12:42:00 +0530

    Update Login_Register_Github_Oauth.py

--- Git_Hub_Oauth/Login_Register_Github_Oauth.py
+++ Git_Hub_Oauth/Login_Register_Github_Oauth.py
@@ -6,8 +6,8 @@ app.secret_key = "githuboauth"
 import os
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 github_blueprint = make_github_blueprint(
-    client_id="a847eb0c987406fb261d",
-    client_secret="b615bb17974dd9410204543e334ae6105ef1645d")
+    client_id="Your Client ID", # github/Profile/Setting/Developer Setting
+    client_secret="Your Client Secret")
 
 app.register_blueprint(github_blueprint, url_prefix="/github_login")
 
